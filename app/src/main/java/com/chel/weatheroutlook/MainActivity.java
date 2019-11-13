9448a9359e95ad54645b1e40824fe66edb61c058
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-11-13 10:31:33 +0200

    added a drawer_menu plus the group items

--- app/src/main/java/com/chel/weatheroutlook/MainActivity.java
+++ app/src/main/java/com/chel/weatheroutlook/MainActivity.java
@@ -1,14 +1,35 @@
 package com.chel.weatheroutlook;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+//    private DrawerLayout mDrawerlayout;
+//    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+//        mDrawerlayout = findViewById( R.id.activity_main);
+//        mToggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open, R.string.close);
+//        mDrawerlayout.addDrawerListener( mToggle);
+//        mToggle.syncState();
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//
+//        if (mToggle.onOptionsItemSelected( item )){
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected( item );
+//    }
 }
