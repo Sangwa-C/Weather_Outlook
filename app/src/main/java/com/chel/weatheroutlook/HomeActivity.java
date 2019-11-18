4a9ba7ebbdf75e14d4e7e21b3ea007048b742d63
kimbagira Sylvie <sylviekimbagira@gmail.com>
2019-11-18 14:27:17 +0200

    added changes

---
+++ app/src/main/java/com/chel/weatheroutlook/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.chel.weatheroutlook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class HomeActivity extends AppCompatActivity {
+    ViewFlipper v_flipper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+        int[] images = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5, R.drawable.img6,R.drawable.img7};
+        v_flipper = findViewById(R.id.v_flipper1);
+
+        for(int image: images){
+            flipperImages(image);
+        }
+
+    }
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000);
+        v_flipper.setAutoStart(true);
+        //annimation
+        v_flipper.setInAnimation(this,android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this,android.R.anim.slide_out_right);
+    }
+
+}
