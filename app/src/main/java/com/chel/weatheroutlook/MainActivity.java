8963187ae5e2b7886228e688a91a851fb8b149f4
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-11-18 12:00:50 +0200

    added the fragments for nav items

--- app/src/main/java/com/chel/weatheroutlook/MainActivity.java
+++ app/src/main/java/com/chel/weatheroutlook/MainActivity.java
@@ -1,14 +1,22 @@
 package com.chel.weatheroutlook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.location.Location;
+import android.net.Network;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout mDrawerlayout;
+    private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
     @Override
@@ -16,11 +24,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
-        mDrawerlayout = findViewById( R.id.activity_main);
-        mToggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open, R.string.close);
-        mDrawerlayout.addDrawerListener( mToggle);
+        mDrawerLayout = findViewById( R.id.activity_main);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener( mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView nvDrawer = (NavigationView) findViewById( R.id.nav_header );
+//        setupDrawerContent(  nvDrawer);
     }
 
     @Override
@@ -32,4 +42,47 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected( item );
     }
+//
+//    public void selectItemDrawer(MenuItem menuItem){
+//        Fragment myFragment = null;
+//        Class fragmentClass;
+//        switch (menuItem.getItemId()){
+//            case R.id.nav_location:
+//                fragmentClass = Edit_location.class;
+//                break;
+//
+//            case R.id.nav_language:
+//                fragmentClass = Language.class;
+//                break;
+//
+//            case R.id.nav_help:
+//                fragmentClass = Help.class;
+//
+//            default:
+//                fragmentClass = Edit_location.class;
+//        }
+//        try {
+//            myFragment = (Fragment) fragmentClass.newInstance();
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction().replace(R.id.f1content,myFragment).commit();
+//        menuItem.setChecked( true );
+//        setTitle(menuItem.getTitle());
+//        mDrawerLayout.closeDrawers();
+//    }
+//
+//    private void setupDrawerContent(NavigationView navigationView){
+//        navigationView.setNavigationItemSelectedListener( new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                selectItemDrawer(menuItem);
+//                return true;
+//            }
+//        } );{
+//
+//        }
+//    }
 }
