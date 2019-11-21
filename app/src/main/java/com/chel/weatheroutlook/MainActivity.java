9eb27ae55ddaa96d75db74ba0b206d50bb7f3d92
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-11-21 14:14:17 +0200

    editid for the menu activity

--- app/src/main/java/com/chel/weatheroutlook/MainActivity.java
+++ app/src/main/java/com/chel/weatheroutlook/MainActivity.java
@@ -4,24 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
-import android.location.Location;
-import android.net.Network;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
-
-import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+//    public  nav_rate
 //    @BindView(R.id.clothesTextView) TextView mClothesTextView;
 
     @Override
@@ -55,44 +48,58 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected( item );
     }
 
-    public void selectItemDrawer(MenuItem menuItem){
-        Fragment myFragment = null;
-        Class fragmentClass;
-        switch (menuItem.getItemId()){
-            case R.id.nav_location:
-                fragmentClass = Edit_location.class;
-                break;
-
-            case R.id.nav_language:
-                fragmentClass = Language.class;
-                break;
-
-            case R.id.nav_settings:
-                fragmentClass = Settings.class;
-                break;
-
-            case R.id.nav_help:
-                fragmentClass = Help.class;
-                break;
-
-            case R.id.nav_rate:
-                fragmentClass = Rate.class;
-                break;
-
-            default:
-                fragmentClass = Edit_location.class;
-        }
-        try {
-            myFragment = (Fragment) fragmentClass.newInstance();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.f1content,myFragment).commit();
+    public boolean selectItemDrawer(MenuItem menuItem){
+//        Fragment myFragment = null;
+//        Class fragmentClass;
+//        switch (menuItem.getItemId()){
+//            case R.id.nav_location:
+//                fragmentClass = Edit_location.class;
+//                break;
+//
+//            case R.id.nav_language:
+//                fragmentClass = Language.class;
+//                break;
+//
+//            case R.id.nav_settings:
+//                fragmentClass = Settings.class;
+//                break;
+//
+//            case R.id.nav_help:
+//                fragmentClass = Help.class;
+//                break;
+//
+//            case R.id.nav_rate:
+//                fragmentClass = Rate.class;
+//                break;
+//
+//            default:
+//                fragmentClass = Edit_location.class;
+//        }
+//        try {
+//            myFragment = (Fragment) fragmentClass.newInstance();
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction().replace(R.id.f1content,myFragment).commit();
         menuItem.setChecked( true );
         setTitle(menuItem.getTitle());
         mDrawerLayout.closeDrawers();
+
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_language){
+            Intent intent = new Intent( MainActivity.this, Language_1.class );
+            startActivity( intent );
+            return true;
+        } else if (id == R.id.nav_rate){
+            Intent intent = new Intent( MainActivity.this, rate.class );
+            startActivity( intent );
+            return true;
+        }
+
+        return false;
     }
 
     private void setupDrawerContent(NavigationView navigationView){
