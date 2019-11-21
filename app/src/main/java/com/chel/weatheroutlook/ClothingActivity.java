74c7504c130bc2b037e73dbb271915b01fd7dbed
Eliane-5 <irankundaeliane9@gmail.com>
2019-11-21 18:31:44 +0200

    adding male avatars and corresponding conditions for clothing

--- app/src/main/java/com/chel/weatheroutlook/ClothingActivity.java
+++ app/src/main/java/com/chel/weatheroutlook/ClothingActivity.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ClothingActivity extends AppCompatActivity {
     public static final String TAG = ClothingActivity.class.getSimpleName();
+    @BindView(R.id.avatar) ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String description = intent.getStringExtra("description");
@@ -23,5 +28,55 @@ public class ClothingActivity extends AppCompatActivity {
 
         Double mMaxTemp = Double.parseDouble(maxStringTemp);
         Double mMinTemp = Double.parseDouble(minStringTemp);
+
+        switch(gender){
+            case "male":
+                if (mMaxTemp<=5.000){
+                    int imageResource = getResources().getIdentifier("@drawable/harshwinter",null,this.getPackageName());
+                    mImage.setImageResource(imageResource);
+                }
+                else if (mMinTemp>5.000 && mMaxTemp<=10.900){
+                    int imageResource = getResources().getIdentifier("@drawable/winter",null,this.getPackageName());
+                    mImage.setImageResource(imageResource);
+                }
+                else if (mMinTemp>10.900 && mMaxTemp<=20.900){
+                    int imageResource = getResources().getIdentifier("@drawable/cold",null,this.getPackageName());
+                    mImage.setImageResource(imageResource);
+                }
+                else if (mMinTemp>20.900 && mMaxTemp<=25.900){
+                    int imageResource = getResources().getIdentifier("@drawable/warm",null,this.getPackageName());
+                    mImage.setImageResource(imageResource);
+                }
+                else if (mMinTemp>25.900 && mMaxTemp<=30.900){
+                    int imageResource = getResources().getIdentifier("@drawable/casual",null,this.getPackageName());
+                    mImage.setImageResource(imageResource);
+                }
+                else if (mMinTemp>30.900 && mMaxTemp<=35.900){
+                    int imageResource = getResources().getIdentifier("@drawable/sunny",null,this.getPackageName());
+                    mImage.setImageResource(imageResource);
+                }
+                else if (mMaxTemp>35.900){
+                    int imageResource = getResources().getIdentifier("@drawable/extrasunny",null,this.getPackageName());
+                    mImage.setImageResource(imageResource);
+                }
+                else {
+                    Log.d(TAG," error male");
+                }
+                break;
+
+            case "female":
+                if (mMinTemp>=20.0 && mMaxTemp<=30.0){
+                    int imageResource = getResources().getIdentifier("@drawable/femaleprofile",null,this.getPackageName());
+                    mImage.setImageResource(imageResource);
+                }
+                else {
+                    Log.d(TAG," female not set yet");
+                }
+            break;
+
+            default:
+                Log.d(TAG,"Error");
+                break;
+        }
     }
 }
